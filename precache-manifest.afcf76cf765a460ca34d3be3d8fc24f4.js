7dd07c68f348bf5241ad0c834a4bfbe49c6d0fad
MCiappina <mciappops@gmail.com>
2021-02-04 15:43:21 -0300

    Updates

---
+++ precache-manifest.afcf76cf765a460ca34d3be3d8fc24f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4855dd2750c8ccadb1ff93168be323a1",
+    "url": "/2Sow-Test-seedS/index.html"
+  },
+  {
+    "revision": "1507ae591d22675343e3",
+    "url": "/2Sow-Test-seedS/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "35cab557192fd00f841f",
+    "url": "/2Sow-Test-seedS/static/css/main.96569932.chunk.css"
+  },
+  {
+    "revision": "1507ae591d22675343e3",
+    "url": "/2Sow-Test-seedS/static/js/2.ba4176ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/2Sow-Test-seedS/static/js/2.ba4176ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cab557192fd00f841f",
+    "url": "/2Sow-Test-seedS/static/js/main.093666a5.chunk.js"
+  },
+  {
+    "revision": "ba3f6cababd4064a132f",
+    "url": "/2Sow-Test-seedS/static/js/runtime-main.6b23996c.js"
+  },
+  {
+    "revision": "c8a268b0a70941ea99dae80b9b07a17a",
+    "url": "/2Sow-Test-seedS/static/media/deleteIcon.c8a268b0.png"
+  },
+  {
+    "revision": "81027c55817f0ef0711983730444ab6e",
+    "url": "/2Sow-Test-seedS/static/media/giphy.81027c55.gif"
+  },
+  {
+    "revision": "cd7b209e2398d60dea3dbe6cf28f6c56",
+    "url": "/2Sow-Test-seedS/static/media/seeds2.cd7b209e.gif"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/2Sow-Test-seedS/static/media/spinner.0d0345f6.gif"
+  }
+]);
